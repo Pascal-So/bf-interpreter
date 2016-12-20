c85af39e49fea55c3920b6a97ebe732b5e5b63f6
Pascal Sommer <p@pascalsommer.ch>
2016-12-20 16:56:44 +0100

    Add Bf data type

--- bf-generator.hs
+++ bf-generator.hs
@@ -1,13 +1,30 @@
-intsp :: a ++ [[a]] -> [a]
-intsp a lst =
-  tail $ foldr (\x acc -> a:x:acc) [] lst
-
+import Data.List
 
 diffs :: (Num a) => [a] -> [a]
-diffs x = if not.null $ x then map (uncurry (-)) $ zip (tail x) x else []
+diffs x = if not.null x then map (uncurry (-)) $ zip (tail x) x else []
 
 roundtrip :: Num a => [a] -> [a]
 roundtrip x = diffs (0:x ++ [0])
 
+shift :: Int -> String
+shift n = if n > 0 then replicate n '>' else replicate (-n) '<'
+
 move :: [Int] -> String
-move x = "[" ++ (intsp '+' $ map shift $ roundtrip x) ++ "-]"
+move x = "[" ++ (intercalate "+" $ map shift $ roundtrip x) ++ "-]"
+
+
+
+
+data Bf = Bf { code :: String
+             , start :: Int
+             , end :: Int
+             }
+
+(<+>) :: Bf -> Bf -> Bf
+Bf {code=ca, start=sa, end=ea} <+> Bf {code =cb, start=sb, end=eb} =
+  let
+    sab = shift (sb - ea)
+    merged = ca ++ sab ++ cb
+  in
+    Bf {code = merged, start=sa, end=sb}
+
